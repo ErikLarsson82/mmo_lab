6d85ce92194d547405eaa25821197cf38040bb48
Erik Larsson <erik.mattias.larsson@gmail.com>
2016-10-25 12:18:38 +0200

    Switched path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 console.log(__dirname, typeof game);
 
 app.get('/', function(req, res){
-  res.sendFile('/client.html');
+  res.sendFile(__dirname + 'client.html');
 });
 
 app.get('/game.js', function(req, res){
